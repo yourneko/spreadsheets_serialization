838860512cb7503afb64880a30efcc21b135d921
Viktor Kharchenko <vkharchenko.dev@gmail.com>
2021-06-26 03:35:36 +0300

    deleted old stuff

--- Assets/Scripts/Spreadsheets/Attributes/RangeAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mimimi.SpreadsheetsSerialization
-{
-    public class RangeAttribute : MapSpaceAttribute
-    {
-        public override SpaceRequired RequiredSpace => SpaceRequired.Range;
-
-        public string DefaultSheet = string.Empty;
-    }
-}
